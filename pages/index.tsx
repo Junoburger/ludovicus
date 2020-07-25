5730c7ebf022605bf8cab226c19add189c872138
Juno <juno.burger@pm.me>
2020-07-25 15:41:00 +0200

    adding typescript and styled-components

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import { Title } from "./components/styled/Header";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Title>Statuut</Title>
+      <h1>Ludovicus</h1>
+    </>
+  );
+}
