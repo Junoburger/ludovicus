996600f664375f6c47c88358f3295abea072cf0b
Juno <juno.burger@pm.me>
2020-07-25 19:40:01 +0200

    added table of contents

--- components/container/TableOfContents.tsx
+++ components/container/TableOfContents.tsx
@@ -1,26 +1,78 @@
 import React from "react";
-import { Contents } from "../styled/TableOfContents";
+import { Contents, ContentOptions, ContentOption } from "../styled/TableOfContents";
 
 export const TableOfContents = () => {
   return (
     <>
       <Contents>
         <h1>INHOUDSOPGAVE</h1>
-        <h3>
-          <i>DE KERN</i>
-        </h3>
-        <h3>
-          <i>DE WERKING VAN HET SPEL: ESSENTIËLE ASPECTEN</i>
-        </h3>
-        <h3>
-          <i>DE WERKING VAN HET SPEL: EXISTENTIËLE ASPECTEN</i>
-        </h3>
-        <h3>
-          <i>HET VERHAAL. . .</i>
-        </h3>
-        <h3>
-          <i>HET VRAAGTEKEN OP HET EIND</i>
-        </h3>
+        <ContentOptions>
+          <h3>
+            <i>DE KERN</i>
+            <ContentOption>
+              <span>1.</span>
+              HET SPEL
+            </ContentOption>
+          </h3>
+        </ContentOptions>
+        <ContentOptions>
+          <h3>
+            <i>DE WERKING VAN HET SPEL: ESSENTIËLE ASPECTEN</i>
+            <ContentOption>
+              <span>2.</span> SPEELSTIJLEN
+            </ContentOption>
+            <ContentOption>
+              <span>3.</span> SPEELMODI
+            </ContentOption>
+            <ContentOption>
+              <span>4.</span> ROLVERDELING
+            </ContentOption>
+            <ContentOption>
+              <span>5.</span> SPEELRICHTINGEN
+            </ContentOption>
+          </h3>
+        </ContentOptions>
+        <ContentOptions>
+          <h3>
+            <i>DE WERKING VAN HET SPEL: EXISTENTIËLE ASPECTEN</i>
+            <ContentOption>
+              <span>6.</span> SPEELSTIJLEN (2)
+            </ContentOption>
+            <ContentOption>
+              <span>7.</span> SPEELMODI (2)
+            </ContentOption>
+            <ContentOption>
+              <span>8.</span> ROLVERDELING (2)
+            </ContentOption>
+          </h3>
+        </ContentOptions>
+        <ContentOptions>
+          <h3>
+            <i>HET VERHAAL. . .</i>
+            <ContentOption>
+              <span>9.</span> ‘GOD’ (HET GOEDE)
+            </ContentOption>
+            <ContentOption>
+              <span>10.</span> HET ‘MISVERSTAND’ (DE HEL)
+            </ContentOption>
+            <ContentOption>
+              <span>11.</span> DE ‘DOODDOENER’ (HET KWAAD)
+            </ContentOption>
+            <ContentOption>
+              <span>12.</span> DEUGDEN EN ZONDEN
+            </ContentOption>
+            <ContentOption>
+              <span>13.</span> HET BOEK DER SPREUKEN
+            </ContentOption>
+          </h3>
+        </ContentOptions>
+        <ContentOptions>
+          <h3>
+            <ContentOption>
+              <span>?.</span> HET VRAAGTEKEN OP HET EIND
+            </ContentOption>
+          </h3>
+        </ContentOptions>
       </Contents>
     </>
   );
--- components/styled/TableOfContents.ts
+++ components/styled/TableOfContents.ts
@@ -1,5 +1,10 @@
 import styled from "styled-components";
 
+export const ContentOptions = styled.div`
+  position: relative;
+`;
+export const ContentOption = styled.div``;
+
 export const Contents = styled.div`
   display: flex;
   justify-content: flex-start;
@@ -7,5 +12,18 @@ export const Contents = styled.div`
   margin-left: 10vw;
   h1 {
     font-size: 5vw;
+  }
+  ${ContentOptions} h3 {
+    display: flex;
+    justify-content: flex-start;
+    flex-direction: column;
+  }
+  ${ContentOption} div {
+    display: flex;
+    justify-content: flex-start;
+    flex-direction: column;
+  }
+  ${ContentOption} span {
+    margin-right: 25px;
   }
 `;
