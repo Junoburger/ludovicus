17bf7e04c8770c61939ea807120b377a772c4d79
Juno <juno.burger@pm.me>
2020-07-25 23:03:02 +0200

    header

--- components/styled/Header.ts
+++ components/styled/Header.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const HeaderContainer = styled.h1`
+export const HeaderContainer = styled.div`
   display: flex;
   justify-content: center;
   h1 {
