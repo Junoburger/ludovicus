b62dd966ee0c33b3d9d2f170872fdcee927e43cf
Juno <juno.burger@pm.me>
2020-08-29 18:39:13 +0200

    added new chapter with some temp inline styles to get mvp online

--- components/container/RoleDivision.tsx
+++ components/container/RoleDivision.tsx
@@ -62,15 +62,88 @@ const FourTwo = () => {
   );
 };
 
-export const RoleDivision = () => {
+const EightOne = () => {
+  return (
+    <>
+      <div>
+        8.1 &nbsp; Een rol in een bepaalde speelmodus &nbsp;
+        <strong>
+          <i>is</i>
+        </strong>
+        &nbsp; een ('nieuwe') rol:
+      </div>
+      <ul>
+        <div style={{ display: "flex" }}>
+          <li>Zelfbewuste Protagonist</li>
+          <span style={{ marginLeft: "5%" }}>(zbs) + (Pr)= (ZbPr)</span>
+        </div>
+        <div style={{ display: "flex" }}>
+          <li>Zelfbewuste Antagonist</li>
+          <span style={{ marginLeft: "5%" }}>(zbs) + (An) = (ZbAn)</span>
+        </div>
+        <div style={{ display: "flex" }}>
+          <li>Zelfbewuste Tritagonist</li>
+          <span style={{ marginLeft: "5%" }}>(zbs) + (Tr)= (ZbTr)</span>
+        </div>
+        <div style={{ display: "flex" }}>
+          <li>Zelfbewuste Bijrol</li>
+          <span style={{ marginLeft: "5%" }}>(zbs) + (B) = (ZbB)</span>
+        </div>
+        <div style={{ display: "flex" }}>
+          <li>Zelfbewuste Figurant</li>
+          <span style={{ marginLeft: "5%" }}>(zbs) + (F)= (ZbF)</span>
+        </div>
+      </ul>
+    </>
+  );
+};
+
+const EightTwo = () => {
+  return (
+    <>
+      <div style={{ display: "flex" }}>
+        8.2 &nbsp;
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <span>
+            ONTWAKEN: (ZbF) {">"} (ZbB) {">"} (ZbTr) {">"} (ZbAn) {">"} (ZbPr)
+          </span>
+          <span>
+            INDOEZELEN: (ZbPr){">"}
+            (ZbAn) {">"} (ZbTr) {">"} (ZbB) {">"} (ZbF)
+          </span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+const ChapterEight = () => {
+  return (
+    <>
+      <h1>
+        <span>8.</span>ROLVERDELING (2)
+      </h1>
+      <EightOne />
+      <EightTwo />
+    </>
+  );
+};
+
+export const RoleDivision = ({ ...props }) => {
   return (
     <>
       <RolesContainer>
-        <h1>
-          <span>4.</span>ROLVERDELING
-        </h1>
-        <FourOne />
-        <FourTwo />
+        {!props.second ? (
+          <>
+            <h1>
+              <span>4.</span>ROLVERDELING
+            </h1>
+            <FourOne />
+            <FourTwo />
+          </>
+        ) : (
+          <ChapterEight />
+        )}
       </RolesContainer>
     </>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -25,6 +25,7 @@ export default function Home() {
       <GameDirections />
       <PlayStyles second />
       <GameModes second />
+      <RoleDivision second />
     </>
   );
 }
